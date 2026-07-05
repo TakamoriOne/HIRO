e519c107c28ad71ba93c4c186655d380b0ddeccd
Joseph Casebeer <joseph@takamorirobot.com>
2026-07-04 20:54:37 -0700

    Added comments to HIRO_Easy

--- src/HIRO_Emotes.cpp
+++ src/HIRO_Emotes.cpp
@@ -1,17 +1,4 @@
-/*
- * File Name: HIRO_Emotes.cpp 
- * Description: Library for containing the emote motion choreography for H.I.R.O. Model TR-01-A  Unit by Takamori Robot
- * Author: Joseph Casebeer, Takamori Robot
- * Date Created: December 31st 2025.
- * Website: https://www.takamorirobot.com
- * 
- * Instructions: The main functions for each emote are what you can use in your sketch. 
- * The supporting functions are used by the main functions and are not intended to be used in your sketch.
- * 
- * 
- * Choreographed movements 
- * 1. wave - HIRO waves its right arm
- */
+
 
 
 #include <HIRO.h>
